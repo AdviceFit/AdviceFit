b589c5a844c27bf202890750e20ad736c98ab41b
SKIT2211 <shyamkasundra2001@gmail.com>
2025-04-05 00:56:18 +0530

    test server backend

--- backend/src/utils/payment.utils.js
+++ backend/src/utils/payment.utils.js
@@ -1,7 +1,7 @@
 const Razorpay = require("razorpay");
 
 const RazorPayInstance = new Razorpay({
-  key_id: process.env.RAZORPAY_CLIENT_ID,
+  key_id: process.env.RAZORPAY_CLIENT_ID || "Razorpay",
   key_secret: process.env.RAZORPAY_CLIENT_SECRET,
 });
 
