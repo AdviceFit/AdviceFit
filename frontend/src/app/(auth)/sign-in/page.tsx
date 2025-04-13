2ac7634c34a8f51468a003f1f5615669710dd084
Soham18 <patelsoham924@gmail.com>
2025-04-13 18:23:58 +0530

    Test 1

--- frontend/src/app/(auth)/sign-in/page.tsx
+++ frontend/src/app/(auth)/sign-in/page.tsx
@@ -4,7 +4,7 @@ const SignInRoute = () => {
   return (
     <section className="p-8 w-full lg:w-2/3 mx-auto h-screen flex flex-col items-center justify-center">
       <div className="w-full">
-        <h2 className="mb-4 text-center">Signin</h2>
+        <h2 className="mb-4 text-center">Signin Test</h2>
         <SignInPage />
       </div>
     </section>
